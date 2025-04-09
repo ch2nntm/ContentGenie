04c108b3f1b257d87b5a971da6083a2665cf5ba1
Minh Chan <minhchan@int-ntmchan-macbook.local>
2025-04-09 16:59:33 +0700

    update04 0904

--- src/single_file/Topbar.tsx
+++ src/single_file/Topbar.tsx
@@ -14,7 +14,7 @@ const Topbar = () => {
     <div style={{ position: 'fixed', display: 'flex', justifyContent: 'space-between', padding: '10px', right: 0, zIndex: 1000, top: 10 }}>
       <select onChange={(e) => setLanguage(e.target.value)} value={language}>
         <option value="en">English</option>
-        <option value="vi">VietNamese</option>
+        <option value="vi">Vietnamese</option>
       </select>
     </div>
   );
